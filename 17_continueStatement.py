49b68aba75a7ee34c89cc39da85d5e86aa267dc0
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-12 23:35:32 +0700

    Add files via upload

---
+++ 17_continueStatement.py
@@ -0,0 +1,14 @@
+for letter in 'Python Programming':
+    if letter == 'g':
+        continue
+    print('Letter is:', letter)
+
+#--------------------------------------------
+
+var = 15
+while var > 0:
+    var = var - 1
+    if var == 10:
+        continue
+    print('Current variable value :', var)
+print("End of Program") 
