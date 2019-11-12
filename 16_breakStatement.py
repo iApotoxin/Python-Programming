27c91c8e866d0f5c3f92c890d8d3c2450de1b211
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-12 19:23:11 +0700

    Add files via upload

---
+++ 16_breakStatement.py
@@ -0,0 +1,14 @@
+for letter in 'Python Programming':
+    if letter == 'g':
+        break
+    print('Letter is:', letter)
+
+#--------------------------------------------
+
+var = 15
+while var > 0:
+    print('Current variable value :', var)
+    var = var - 1
+    if var == 10:
+        break
+print("End of Program")  
