218d507e08054e7a682fa85d8f972b2251f323c9
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-13 12:05:10 +0700

    Add files via upload

---
+++ 28_Tuple2.py
@@ -0,0 +1,23 @@
+myTuple1 = (("Name", "Ratchanok Intanon"),("Age","24"),("Gender","Female"))
+
+print(myTuple1[0])
+print(myTuple1[1])
+print(myTuple1[2])
+
+#----------------------------------------------------------------
+myTuple2 = ('Liverpool', 'Manchester City', 2019, 2018)
+print (myTuple2)
+
+del myTuple2
+print ("After Deleted: ",myTuple2)
+
+#name 'myTuple2' is not defined
+
+#----------------------------------------------------------------
+myTuple3 = (50, 100, 150)
+myTuple3[0] = 200
+
+#'tuple' object does not support item assignment
+#----------------------------------------------------------------
+
+
