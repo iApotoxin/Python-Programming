a201661c72d3665718fd231749fe540853bc9757
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-13 21:33:48 +0700

    Add files via upload

---
+++ 34_DictionaryFor.py
@@ -0,0 +1,7 @@
+myDict1 = {1:"Data Analytic", 2:'Cloud Computing', 3:"Big Data", 4:"IOT"}
+for i in myDict1:
+    print(myDict1[i])
+
+myDict2 = {1:"Python", 2:(100, 200), '3':[300, 400, 500], 4:{"Name":"Tizcom"}}
+for i in myDict2:
+    print(myDict2[i])
---
+++ 35_DictionarySum.py
@@ -0,0 +1,7 @@
+myDict = {'Subject': 'C#', 'PGA': 3.5, 'Subject': 'Java'}
+print ("myDict['Subject']: ", myDict['Subject'])
+
+# Key Should be Unique key but in real case python not verify
+# If in Dictionary have dupicate key , python choose last value
+
+# In real world Key Should be not change
