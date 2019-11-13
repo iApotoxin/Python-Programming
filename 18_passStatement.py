7e64ac9e1bbfb8e7ed07403ff143c3a216a4fe71
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-13 10:43:32 +0700

    Delete 18_passStatement.py

--- 18_passStatement.py
+++
@@ -1,7 +0,0 @@
-for letter in 'Python Programming':
-    if letter == 'g':
-        pass
-        print('This is pass block')
-    print('Letter is:', letter)
-
-#switch, do-while, forecach not support in Python
