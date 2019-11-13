28a9d60b61b5819f56adb1ac437efb384a3ebb41
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-13 10:43:48 +0700

    Delete 20_forLoop2.py

--- 20_forLoop2.py
+++
@@ -1,8 +0,0 @@
-for num in range(10, 30):        #to iterate between 10 to 30
-    for i in range(2, num):
-        if num % i == 0:
-            j = num / i
-            print('%d equals %d * %d' % (num, i, j))
-            break
-    else:
-        print(num, 'is a prime number')
