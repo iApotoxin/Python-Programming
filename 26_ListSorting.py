feec8074cda805184905d809f6a481923583405b
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-13 11:12:05 +0700

    Add files via upload

---
+++ 26_ListSorting.py
@@ -0,0 +1,15 @@
+myList = [10,30,50,80,70,40,20,90,60]
+myList.sort()
+print(myList)
+
+#--------------------------------------
+myStringList = ['Python','Java','R']
+myStringList.sort()
+print(myStringList)
+
+#--------------------------------------
+
+#myMixList = ['Data Science','Machine Learning', 2018, 2015]
+#myMixList.sort()
+#print(myMixList)
+##not supported between instances of Number and String
