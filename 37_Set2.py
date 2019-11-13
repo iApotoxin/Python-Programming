5fc7517cb85801d0141a2bf4626257092b97cd74
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-13 22:48:45 +0700

    Add files via upload

---
+++ 37_Set2.py
@@ -0,0 +1,15 @@
+setA = {10, 20, 30, 40, 50}
+
+print('Original Set : ', setA)
+
+setA.remove(50)
+print('Deleted Set : ', setA)
+
+setA.clear()
+print('Clear All Set : ', setA)
+
+setA.update({50,70,90})
+print('Updated Set : ', setA)
+
+
+#----------------------------------------------------
