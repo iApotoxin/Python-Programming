846f54f5dc2922c7267077ca8491db97f402dc93
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-14 14:23:59 +0700

    Add files via upload

---
+++ 54_CallingModule2.py
@@ -0,0 +1,5 @@
+from CalAreaRectangle import rectangle, squre, parallelogram
+
+print("Area of Rectangle :", rectangle(5, 5))
+print("Area of Squre :", squre(5, 10))
+print("Area of Parallelogram :", parallelogram(1.5, 5.5))
