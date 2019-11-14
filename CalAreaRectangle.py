4bc4ecb13768ff8db59de26494b48cd2e7cfed88
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-14 13:49:18 +0700

    Add files via upload

---
+++ CalAreaRectangle.py
@@ -0,0 +1,11 @@
+def rectangle(width, height):
+    return width * height
+
+def squre(width1, width2):
+    return width1 * width2
+
+def parallelogram(height, base):
+    return height * base
+
+def trapezoid(sumofpararell, height):
+    return 0.5 * sumofpararell * height
