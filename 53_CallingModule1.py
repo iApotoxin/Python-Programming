f112712bd7c8247aea1cbf1440c54e03378c78c1
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-14 14:10:39 +0700

    Add files via upload

---
+++ 53_CallingModule1.py
@@ -0,0 +1,6 @@
+import CalAreaRectangle
+
+print("Area of Rectangle :",CalAreaRectangle.rectangle(3, 4))
+print("Area of Squre :",CalAreaRectangle.squre(3, 3))
+print("Area of Parallelogram :",CalAreaRectangle.parallelogram(10.5, 5.5))
+print("Area of Trapezoid :",CalAreaRectangle.trapezoid(5, 5.5))
