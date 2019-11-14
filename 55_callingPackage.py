6c4fadff45ad391ffe5637acbd63df412ad401ca
Tizcom <45397970+Tizcom@users.noreply.github.com>
2019-11-14 14:56:59 +0700

    Add files via upload

---
+++ 55_callingPackage.py
@@ -0,0 +1,6 @@
+import CalArea
+
+print("Area of squre2 :",CalArea.squre2(2.5,2.5))
+print("Area of rectangle2 :",CalArea.rectangle2(2.5, 5.5))
+print("Area of triangle2 :",CalArea.triangle2(5, 2.5))
+print("Area of equilateral2 :",CalArea.equilateral2(5.0))
---
+++ CalAreaTriangle.py
@@ -0,0 +1,11 @@
+def triangle(height, base):
+    return 1/2 * height * base
+
+def equilateral(width):
+    return 3**(1/2) * width * width
+
+def isosceles(base, sidebase):
+    return base/4 * 4 * (((sidebase*sidebase) - (base*base)))**(1/2)
+
+def pythagorean(perpendicular):
+    return 0.5 * perpendicular * perpendicular
